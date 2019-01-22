a507439bb0f576edc683203efae6bdcab7ee8527
crileiton <crileiton@umariana.edu.co>
2019-01-22 12:25:54 -0500

    Ejercicio 1 realizado de la practica

---
+++ 5 Entradas y salidas de datos/4 Ejercicios optativos I.py
@@ -0,0 +1,7 @@
+# Desarrollo ejercicios No. 1
+print( " {:>20}".format( "Hola mundo" ) )
+print( " {:.3}".format( "Hola mundo" ) )
+print( " {:^20.1}".format( "Hola mundo" ) )
+print( " {:05d}".format( 150 ) )
+print( " {:7d}".format( 7887 ) )
+print( " {:07.3f}".format( 20.02 ) )
