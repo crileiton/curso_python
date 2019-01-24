f92a7d6d1b4faa656937e736383792e1a9874398
crileiton <crileiton@umariana.edu.co>
2019-01-24 11:56:16 -0500

    Ejercicio avanzado terminado

--- 5 Entradas y salidas de datos/descomposicion.py
+++ 5 Entradas y salidas de datos/descomposicion.py
@@ -3,12 +3,14 @@ import sys
 # Es 2 en la sgte linea porque el nombre del archivo cuenta tambien
 if len(sys.argv) == 2:
     numero = int(sys.argv[1])
-    if (numero > 0):
-        lista_numeros = [numero]
-        print(lista_numeros)
+    if (numero > 0 and numero < 9999):
+        cadena = str(numero)
+        longitud = len(cadena)
+        for i in range(longitud):
+            print("{:04d}".format( int(cadena[longitud-1-i]) * 10 ** i) )
     else:
-        print("El número no es positivo")
-        print("Ejemplo: descomposicion.py [número_entero_positivo]")
+        print("El número no es correcto")
+        print("Ejemplo: descomposicion.py [0 - 9999]")
 else:
     print("Error argumentos incorrectos.")
-    print("Ejemplo: descomposicion.py [número_entero_positivo]")
+    print("Ejemplo: descomposicion.py [0 - 9999]")
