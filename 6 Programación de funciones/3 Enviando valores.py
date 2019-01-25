70ad33be3bd58b46ebdbe6a0c18c79bb8b3a6c19
crileiton <crileiton@umariana.edu.co>
2019-01-25 11:25:28 -0500

    Enviando valores

---
+++ 6 Programación de funciones/3 Enviando valores.py
@@ -0,0 +1,9 @@
+# Caso de estudio: Crear una funcion que sume dos numeros y devuelva su resultado
+# Respuesta...
+
+def suma(a, b):
+    return a + b
+
+print(suma(4,6))
+
+# Durante la definición los valores de a y b se denominan parametros y durante la llamada 2,5 se denominan argumentos
