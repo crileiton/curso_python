3cf085f7f122c8c2969b3b3d020f0d97e0ab777e
crileiton <crileiton@umariana.edu.co>
2019-01-28 19:37:14 -0500

    Clases y objetos: Clases y objetos

---
+++ 2 Clases y objetos.py
@@ -0,0 +1,17 @@
+# Vamos a definir una clase ...asi
+
+# La clase es como un molde para crear objetos
+class Galleta:
+    pass
+
+# Lo siguiente se denomina instanciación 
+una_galleta = Galleta()
+otra_galleta = Galleta()
+
+# Podemos saber la clase de un objeto o valor gracias a la función type...asi
+print(type(una_galleta))
+# Ejecutando el anterior codigo nos dice que una_galleta es de un tipo algo.galleta asi...<class '__main__.Galleta'>
+# Podemos ver el tipo de cualquier cosa asi...
+# print(type(True))
+
+
